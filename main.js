4c0d3e016b6dc9fdca696233e8ee8d0166fa64c5
Mike van den Bos <stefmike12@gmail.com>
2019-06-08 17:10:06 +0200

    interactive

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $("#san_jose").click(function () {
+        $("#musk_een").toggleClass("mapfoto");
+    });
+});   
+
+$(document).ready(function () {
+    $("#los_angeles").click(function () {
+        $("#musk_twee").toggleClass("mapfoto");
+    });
+});   
+
+
+$(document).ready(function () {
+    $("#los_angeles").click(function () {
+        $("#musk_een").removeClass("mapfoto");
+    });
+});   
+
+$(document).ready(function () {
+    $("#san_jose").click(function () {
+        $("#musk_twee").removeClass("mapfoto");
+    });
+});
+
+
